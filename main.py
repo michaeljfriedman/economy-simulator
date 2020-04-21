ca706d103d8fe9b5aacf870878b2a0b68c07adaf
Michael Friedman <mfred240@gmail.com>
2020-04-21 02:45:30 +0000

    Outlined main executable

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+This is the executable of the project. You can run this to run the simulator
+from the command line.
+
+Example:
+python main.py --config=config.json
+'''
+
+import argparse
+import json
+import sys
+
+def main(argv):
+  print('Not implemented')
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
