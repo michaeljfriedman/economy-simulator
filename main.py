50c844a23aee9b03e5ec1c280c2a228e26bb7d65
Michael Friedman <mfred240@gmail.com>
2020-04-25 23:30:59 +0000

    Implemented main.py (command-line executable)

--- main.py
+++ main.py
@@ -7,11 +7,43 @@ python main.py --config=config.json
 '''
 
 import argparse
+import csv
 import json
+import os
+import simulator
 import sys
 
 def main(argv):
-  print('Not implemented')
+  # Parse config
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-c', '--config', dest='config', type=str,
+    default='config.json', help='The path to the config file')
+  parser.add_argument('-o', '--output', dest='output_dir', type=str,
+    default='output', help='The directory to write results to')
+  args = parser.parse_args()
+  with open(args.config, 'r') as f:
+    config = json.loads(f.read())
+
+  # Run simulator
+  results = simulator.run(
+    npersons=config['npersons'],
+    ncompanies=config['ncompanies'],
+    ndays=config['ndays'],
+    income=config['income'],
+    saving_rate=config['saving_rate']
+  )
+
+  # Convert unemployment results to csv-writable format
+  results['unemployment'] = map(lambda x: [x], results['unemployment'])
+
+  # Write results
+  if not os.path.isdir(args.output_dir):
+    os.mkdir(args.output_dir)
+  for name, data in results.items():
+    output_file = os.path.join(args.output_dir, '%s.csv' % name)
+    with open(output_file, 'w') as f:
+      w = csv.writer(f)
+      w.writerows(data)
 
 if __name__ == '__main__':
   main(sys.argv[1:])
