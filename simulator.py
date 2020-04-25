0862cf99df7410c4216ffeccad2169bc0a643143
Michael Friedman <mfred240@gmail.com>
2020-04-25 17:09:09 +0000

    Implemented simulator initialization

--- simulator.py
+++ simulator.py
@@ -4,89 +4,55 @@ The simulator.
 
 import numpy as np
 
+# A person in the model
+class Person:
+
+  def __init__(self, money=0, income=0, saving_rate=0.3, employed=True):
+    self.money = money
+    self.income = income
+    self.saving_rate = saving_rate
+    self.employed = employed
+
+# A company in the model
+class Company:
+
+  def __init__(self, money=0, employees=[], in_business=True):
+    self.money = money
+    self.employees = employees
+    self.in_business = in_business
+
 # Runs the simulator, given parameters:
-# - npersons: the number of people in the model
-# - ncompanies: the number of companies in the model
-# - ndays: the number of days to run for
-# - dcompanies: the distribution of companies across industries
-# - demployees: the distribution of employees across industries
-# - dincome: the distributions of income in each industry
-# - dcompany_expenses: the distributions of company expenses in each industry
-# - dperson_expenses: the distributions of person expenses in each income bracket
+# - npersons (int): the number of people in the model
+# - ncompanies (int): the number of companies in the model
+# - ndays (int): the number of days to run for
+# - income (int): annual income to apply to all people
+# - saving_rate (int): the saving rate to apply to all people
 #
-# Returns a 2-tuple: (success, results), where `successs` is a boolean
-# indicating whether the run was successful, and `results` is a dict with the
-# values:
-# - err: an error message, if unsuccessful
-# - dperson_wealth: ([wealth brackets], [% of persons in each wealth bracket])
-# - dcompany_wealth: ([wealth bracket], [% of companies in each wealth bracket])
-# - unemployment: the % of persons unemployed
-# - death: the % of persons dead
-# - unemployment_by_industry: {industry: % of persons unemployed in that industry}
-# - death_by_industry: {industry: % of persons dead in that industry}
+# Returns a dict of results:
+# - person_wealth: a list of (min, p10, p25, p50, p75, p90, max) tuples, one for
+#   each day, representing the wealth distribution across people
+# - company_wealth: an analogous list for company wealth
+# - unemployment: a list of unemployment rates, one for each day
 def run(
   npersons=0,
   ncompanies=0,
   ndays=0,
-  dcompanies=(,), # 2-tuple ([industries], [% of companies in each industry])
-  demployees=(,), # 2-tuple ([industries], [% of employees in each industry])
-  dincome={}, # map {industry: ([salaries], [% of employees with each salary])}
-  dcompany_expenses={}, # map {industry: ([other industries], [% of expenses paid to each industry])}
-  dperson_expenses={}): # map {income range: ([industries], [% of expenses paid to each industry])}
-
-  # Do some data validation
-  industries = sorted(dcompanies[0])
-  if sorted(demployees[0]) != industries:
-    return False, {'err': 'Industries in demployees do not match'}
-  if sorted(dincome.keys()) != industries:
-    return False, {'err': 'Industries in dincome do not match'}
-  if sorted(dcompany_expenses.keys()) != industries:
-    return False, {'err': 'Industries in dcompany_expenses do not match'}
-  for _, d in dperson_expenses.items():
-    test_industries, _ = d
-    if sorted(test_industries) != industries:
-      return False, {'err': 'Industries in dperson_expenses do not match'}
+  income=65000,
+  saving_rate=0.3
+  ):
 
   # Set up simulation
-  # TODO
-
-  # Run simulation
-  for i in range(ndays):
-    # TODO: people pay expenses
-
-    # Companies pay expenses
-    for c in companies:
-      if not c.in_business:
-        continue
-
-      for other_c, amount in c.expenses:
-        pay = amount / 360
-        if pay > c.money:
-          # Go out of business
-          for e in c.employees:
-            e.income = 0
-          c.employees = []
-          c.in_business = False
-          break
-        other_c.money += amount / 360
-
-    # Pay employees their income at the end of each month
-    if i % 30 == 29:
-      for c in companies:
-        if not c.in_business:
-          continue
-
-        # Pay employees
-        while True:
-          total_pay = np.sum([e.income / 12 for e in c.employees])
-          if total_pay <= c.money:
-            break
-          # TODO: lay off employee
-          if len(c.employees) == 0:
-            c.in_business = False
-            break
-        for e in c.employees:
-          e.money += e.income / 12
-
-  # TODO: Compute results
-  return True, {'err': ''}
+  people = [
+    Person(
+      money=income/12,
+      income=income,
+      saving_rate=saving_rate
+    ) for i in range(npersons)
+  ]
+  companies = [Company() for i in range(ncompanies)]
+  people_per_company = int(npersons / ncompanies)
+  extra = npersons % ncompanies
+  for i in range(ncompanies): # assign people to companies
+    companies[i].employees = people[i*people_per_company:(i+1)*people_per_company]
+    if i < extra:
+      companies[i].employees.append(people[len(people)-1-i]) # add one extra
