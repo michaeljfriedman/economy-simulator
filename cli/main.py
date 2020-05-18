Removed calculation of results from simulator.py, fix results in plot.py
The simulator no longer computes results; that's left to the client
(main.py / plot.py). Also there was a bug in plot.py - unemployment
and business closure rates were not calculated, so they weren't
plotted correctly. This has been fixed.

cli/main.py
@@ -5,8 +5,8 @@ from the command line. Outputs the results to the specified directory:
 - person_money.csv: 1 row for each day, recording each person's money that day
 - person_industries.csv: 1 row for each day, recording each person's industry
   that day
-- person_employment.csv: 1 row for each day, recording 0/1 whether each person
-  is employed.
+- person_unemployment.csv: 1 row for each day, recording 0/1 whether each person
+  is unemployed.
 - company_industries.csv: A row of each company's industry
 - company_money.csv: 1 row for each day, recording each company's money that day
 - company_closures.csv: 1 row for each day, recording 0/1 whether each company
@@ -41,19 +41,19 @@ def main(argv):
   person_income = [[]]
   person_money = [[] for i in range(total_ndays)]
   person_industries = [[] for i in range(total_ndays)]
-  person_employment = [[] for i in range(total_ndays)]
+  person_unemployment = [[] for i in range(total_ndays)]
   company_industries = [[]]
   company_money = [[] for i in range(total_ndays)]
   company_closures = [[] for i in range(total_ndays)]
   t = tqdm(total=total_ndays)
-  def update_progress(period, day, people, companies, results):
+  def on_eod(period, day, people, companies):
     i = simulator.days_per_month * period + day
     for p in people:
       if period == 0 and day == 0:
         person_income[0].append(p.income)
       person_money[i].append(p.money)
       person_industries[i].append(p.industry)
-      person_employment[i].append(p.employed)
+      person_unemployment[i].append(not p.employed)
     for c in companies:
       if period == 0 and day == 0:
         company_industries[0].append(c.industry)
@@ -61,12 +61,12 @@ def main(argv):
       company_closures[i].append(not c.in_business)
     t.update()
 
-  results = simulator.run(
+  simulator.run(
     ncompanies=config['ncompanies'],
     employees=config['employees'],
     income=config['income'],
     periods=config['periods'],
-    update_progress=update_progress
+    on_eod=on_eod
   )
   t.close()
 
@@ -77,7 +77,7 @@ def main(argv):
     ('person_income', person_income),
     ('person_money', person_money),
     ('person_industries', person_industries),
-    ('person_employment', person_employment),
+    ('person_unemployment', person_unemployment),
     ('company_industries', company_industries),
     ('company_money', company_money),
     ('company_closures', company_closures)
@@ -89,7 +89,7 @@ def main(argv):
       arr = np.array(data)
       if filename in ['person_income', 'person_money', 'company_money']:
         arr = np.around(arr, 2) # round to 2 decimal places
-      elif filename in ['person_employment', 'company_closures']:
+      elif filename in ['person_unemployment', 'company_closures']:
         arr = arr.astype(int) # convert to 0/1
       w.writerows(arr)
 

cli/plot.py
@@ -16,10 +16,19 @@ import os
 # - names = list of names to compute percentiles for
 # - cols = list of column indexes
 # - percentiles = list of percentiles to compute
-def compute_results(data, names, cols, percentiles):
+def compute_percentiles(data, names, cols, percentiles):
   results = {}
   for name in names:
     results[name] = np.percentile(data[name][:,cols], percentiles, axis=1).T
+  return results
+
+# Computes the average of each row of each dataset, restricted to only the given
+# cols
+def compute_avg(data, names, cols):
+  results = {}
+  for name in names:
+    nrows = data[name].shape[0]
+    results[name] = np.mean(data[name][:,cols], axis=1).reshape((nrows, 1))
   return results
 
 # Plots one set of results to an output file.
@@ -54,7 +63,7 @@ def main():
     'person_income': [],
     'person_money': [],
     'person_industries': [],
-    'person_employment': [],
+    'person_unemployment': [],
     'company_industries': [],
     'company_money': [],
     'company_closures': []
@@ -75,8 +84,8 @@ def main():
   for f in data:
     if f in ['person_income', 'person_money', 'company_money']:
       dtype = np.float
-    elif f in ['person_employment', 'company_closures']:
-      dtype = np.bool
+    elif f in ['person_unemployment', 'company_closures']:
+      dtype = np.int
     else:
       dtype = np.str
     data[f] = np.array(data[f], dtype=dtype)
@@ -91,43 +100,71 @@ def main():
   ncompanies = data['company_money'].shape[1]
 
   # Plot overall results
-  overall_results = compute_results(
+  overall_results = compute_percentiles(
     data=data,
-    names=['person_money', 'person_employment'],
+    names=['person_money'],
     cols=range(npeople),
     percentiles=args.percentiles
   )
-  overall_results.update(compute_results(
+  overall_results.update(compute_avg(
     data=data,
-    names=['company_money', 'company_closures'],
+    names=['person_unemployment'],
+    cols=range(npeople)
+  ))
+  overall_results.update(compute_percentiles(
+    data=data,
+    names=['company_money'],
     cols=range(ncompanies),
     percentiles=args.percentiles
+  ))
+  overall_results.update(compute_avg(
+    data=data,
+    names=['company_closures'],
+    cols=range(ncompanies)
   ))
   plot(x, overall_results, os.path.join(args.directory, 'overall.png'))
 
   # Plot results per income level
   for income in np.unique(data['person_income']):
-    y = compute_results(
+    cols = [j for j in range(npeople) if data['person_income'][0,j] == income]
+    y = compute_percentiles(
       data=data,
-      names=['person_money', 'person_employment'],
-      cols=[j for j in range(npeople) if data['person_income'][0,j] == income],
+      names=['person_money'],
+      cols=cols,
       percentiles=args.percentiles
     )
+    y.update(compute_avg(
+      data=data,
+      names=['person_unemployment'],
+      cols=cols
+    ))
     plot(x, y, os.path.join(args.directory, 'income-%.2f.png' % income))
 
   # Plot results per industry
   for ind in np.unique(data['company_industries']):
-    y = compute_results(
+    cols_people = [j for j in range(npeople) if data['person_industries'][0,j] == ind]
+    cols_companies = [j for j in range(ncompanies) if data['company_industries'][0,j] == ind]
+    y = compute_percentiles(
       data=data,
-      names=['person_money', 'person_employment'],
-      cols=[j for j in range(npeople) if data['person_industries'][0,j] == ind],
+      names=['person_money'],
+      cols=cols_people,
       percentiles=args.percentiles
     )
-    y.update(compute_results(
+    y.update(compute_avg(
       data=data,
-      names=['company_money', 'company_closures'],
-      cols=[j for j in range(ncompanies) if data['company_industries'][0,j] == ind],
+      names=['person_unemployment'],
+      cols=cols_people
+    ))
+    y.update(compute_percentiles(
+      data=data,
+      names=['company_money',],
+      cols=cols_companies,
       percentiles=args.percentiles
+    ))
+    y.update(compute_avg(
+      data=data,
+      names=['company_closures'],
+      cols=cols_companies
     ))
     plot(x, y, os.path.join(args.directory, 'industry-%s.png' % ind))
 

simulator/simulator.py
@@ -200,53 +200,24 @@ def pay_employees(people, companies):
       e.money += amount
   return people, companies
 
-# Calculates statistics based on the current state of the model and adds them
-# to the given results. Returns the new results.
-def calculate_stats(results, people, companies):
-  for ind, ind_results in results.items():
-    ind_people = [p for p in people if p.industry == ind]
-    ind_companies = [c for c in companies if c.industry == ind]
-    percentiles = range(0, 101)
-    new_results = {
-      'person_wealth': [0] * len(percentiles) if len(ind_people) == 0 else list(np.percentile([p.money for p in ind_people], percentiles)),
-      'company_wealth': [0] * len(percentiles) if len(ind_companies) == 0 else list(np.percentile([c.money for c in ind_companies], percentiles)),
-      'unemployment': 1 if len(ind_people) == 0 else len([p for p in ind_people if not p.employed]) / len(ind_people),
-      'out_of_business': 1 if len(ind_companies) == 0 else len([c for c in ind_companies if not c.in_business]) / len(ind_companies)
-    }
-    for k in ind_results.keys():
-      ind_results[k].append(new_results[k])
-  return results
-
 # Runs the simulator, given the parameters as defined in design.md; and an
-# optional callback function update_progress, which is called at the end of each
-# day, with the following arguments:
+# optional callback function on_eod, which is called at the end of each day,
+# with the following arguments:
 # - period: the index of the current period (from 0)
 # - day: the index of the current day (from 0)
 # - people: the list of people
 # - companies: the list of companies
-# - results: the dict of results through the current day, as described below.
-# The caller can use this to report progress up a level.
+# The caller can use this to record data and/or report progress up a level.
 #
 # NOTE: Be mindful - these args are passed by reference, so you can technically
 # change them and mess with the simulation. Please don't do that. Read only. I
 # would have passed a copy instead, but it slows down the simulation a lot.
-#
-# Returns a dict of results. Each key is an industry name from industries, and
-# each value is a dict of:
-# - person_wealth: a list of stats, one for each day, where each day is a list
-#   of every percentile of the wealth distribution across people in that
-#   industry.
-# - company_wealth: an analogous list for company wealth
-# - unemployment: a list of unemployment rates in that industry, one for each
-#   day
-# - out_of_business: a list of out-of-business rates (fraction of companies
-#   in that industry that are out of business), one for each day
 def run(
   ncompanies=defaults['ncompanies'],
   income=defaults['income'],
   employees=defaults['employees'],
   periods=defaults['periods'],
-  update_progress=lambda period, day, people, companies, results: None
+  on_eod=lambda period, day, people, companies: None
   ):
 
   # Set up simulation
@@ -265,12 +236,6 @@ def run(
   industries = None
 
   # Run simluation
-  results = {
-    industry: {
-      'person_wealth': [], 'company_wealth': [], 'unemployment': [], 'out_of_business': []
-    } for industry in industry_names
-  }
-  results = calculate_stats(results, people, companies)
   for i in range(len(periods)):
     # Set parameters for this period
     person_stimulus = person_stimulus if 'person_stimulus' not in periods[i] else periods[i]['person_stimulus']
@@ -300,7 +265,4 @@ def run(
         # Reset people's spending rates
         people = reset_spending_rates(people, spending)
 
-      results = calculate_stats(results, people, companies)
-      update_progress(i, j, people, companies, results)
-
-  return results
+      on_eod(i, j, people, companies)